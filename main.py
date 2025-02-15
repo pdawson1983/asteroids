bbdc4549aab3e52a7e57d77c35ed5beb2a3a2696
Paul Dawson <pdawson1983@gmail.com>
2025-02-15 02:07:05 -0500

    add close display code

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0))
         pygame.display.flip()
 
