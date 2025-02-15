92d31f46e29630258a304f2c09701ee124d31558
Paul Dawson <pdawson1983@gmail.com>
2025-02-15 01:58:56 -0500

    add initial display code

---
+++ main.py
@@ -0,0 +1,20 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
